771d8926d7d15d3418da6568123debd7996281f3
양원준 <109562661+ADEL2006@users.noreply.github.com>
2025-09-01 17:35:41 +0900

    호스팅 설정
    외부접속 허용

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3333, // 원하는 포트 번호로 변경
+  },
 })
