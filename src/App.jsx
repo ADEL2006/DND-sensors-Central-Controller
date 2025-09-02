9ebf02e5f6dbf47e46389db52efbfd28cca0f4f0
양원준 <109562661+ADEL2006@users.noreply.github.com>
2025-09-02 09:22:43 +0900

    원 트레일 추가
    원 애니메이션에 트레일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-// src/App.jsx
 import { useRadarSocket } from './data/RadarSocket';
 import Radar from './components/Radar';
 import Record from './components/Record';
 import Video from './components/Video';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 export default function App() {
     const { wsStatus, dataArray } = useRadarSocket();
+    const [statusColor, setStatusColor] = useState("red"); // 기본값 red
+
+    // wsStatus 변경 시 색상 업데이트
+    useEffect(() => {
+        if (wsStatus === "connected") {
+            setStatusColor("lime");
+        } else {
+            setStatusColor("red");
+        }
+    }, [wsStatus]);
 
     return (
         <div className='main'>
             <div className='main_title'>
-                <h1>WebSocket 상태: {wsStatus}</h1>
+                <h1>
+                    WebSocket 상태: <span style={{ color: statusColor }}>{wsStatus}</span>
+                </h1>
             </div>
             <div className='contents'>
                 <Radar dataArray={dataArray} />
-                <div className='right-element'>
+                <div className='right_element'>
                     <Video />
                     <Record dataArray={dataArray} />
                 </div>
