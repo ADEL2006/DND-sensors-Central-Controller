db63fedc410ef8b6f8a5c118ce49f75ed515d5f5
양원준 <109562661+ADEL2006@users.noreply.github.com>
2025-09-11 15:00:03 +0900

    비디오 로딩/에러 도중에 NO SIGNAL 글자 추가

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -5,6 +5,7 @@ function Video() {
     const canvasRef = useRef(null);
     const playerRef = useRef(null);
     const [ready, setReady] = useState(false);
+    const [hasSignal, setHasSignal] = useState(false);
 
     // CDN JSMpeg 준비 확인
     useEffect(() => {
@@ -22,6 +23,8 @@ function Video() {
         playerRef.current = new window.JSMpeg.Player('ws://58.79.238.184:4001', {
             canvas: canvasRef.current,
             autoplay: true,
+            onPlay: () => setHasSignal(true),   // 영상 재생 시작
+            onPause: () => setHasSignal(false), // 신호 끊김
         });
 
         return () => {
@@ -32,7 +35,12 @@ function Video() {
     return (
         <div className='livqCQ'>
             <h2 className='video_title'>Video</h2>
-            <canvas ref={canvasRef} className='video'></canvas> 
+            <div className="video_wrapper">
+                <canvas ref={canvasRef} className='video'></canvas> 
+                <span className="no_signal" style={{ display: hasSignal ? 'none' : 'block' }}>
+                    NO SIGNAL
+                </span>
+            </div>
         </div>
     );
 }
