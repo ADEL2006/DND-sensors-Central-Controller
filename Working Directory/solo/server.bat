d57aa697c0181c5c40e00e164405fc4a344518bd
양원준 <109562661+ADEL2006@users.noreply.github.com>
2025-10-16 17:44:32 +0900

    서버 실행 파일 수정

--- Working Directory/solo/server.bat
+++ Working Directory/solo/server.bat
@@ -1,12 +1,15 @@
 @echo off
+chcp 65001
 
-REM 프로젝트 폴더로 이동
 cd /d "C:\Users\DNET-shin\Desktop\server projects\DNS-radar-Server"
+
+REM 서버 시작 시간
+set START_TIME=%DATE:~0,4%%DATE:~5,2%%DATE:~8,2%_%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%
 
 REM Gradle 빌드
 call gradlew.bat clean build
 
-REM JAR 파일 절대 경로
+REM JAR 파일 경로
 set JAR_NAME="C:\Users\DNET-shin\Desktop\server projects\DNS-radar-Server\build\libs\radar-0.0.1-SNAPSHOT.jar"
 
 echo.
@@ -14,7 +17,7 @@ echo ============================
 echo Running Spring Boot server...
 echo ============================
 
-REM Spring Boot 실행
-java -jar %JAR_NAME% --server.port=4000 --spring.profiles.active=prod
+REM 서버 실행 (UTF-8 + START_TIME 전달)
+java -Dfile.encoding=UTF-8 -DSTART_TIME=%START_TIME% -jar %JAR_NAME% --server.port=4000 --spring.profiles.active=prod
 
 pause
